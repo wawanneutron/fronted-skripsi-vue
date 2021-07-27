7f107db42eaaa772172c4a0fb826f91f68b422f6
wawanneutron <wawanneutron1331@gmail.com>
2021-07-27 21:02:42 +0700

    update dashboard

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -4,7 +4,9 @@
     <div class="dashboard-customer">
       <div class="dashboard-heading">
         <h2 class="dashboard-title">Transactions</h2>
-        <p class="dashboard-subtitle">Big result start from the small one</p>
+        <p class="dashboard-subtitle">
+          Nikmati kemudahan berbelanja dalam genggamanmu
+        </p>
       </div>
       <!-- breadcrumb -->
       <section class="mt-global store-breadcrumbs">
@@ -25,170 +27,99 @@
       <div class="dashboard-content">
         <div class="dashboard-heading"></div>
         <!-- list transactions -->
-        <div class="row mt-3">
-          <div class="col-12 mt-2">
-            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-              <li class="nav-item" role="presentation">
-                <a
-                  class="nav-link active"
-                  id="pills-home-tab"
-                  data-toggle="pill"
-                  href="#pills-home"
-                  role="tab"
-                  aria-controls="pills-home"
-                  aria-selected="true"
-                  >My Dashboard</a
-                >
-              </li>
-              <li class="nav-item" role="presentation">
-                <a
-                  class="nav-link"
-                  id="pills-profile-tab"
-                  data-toggle="pill"
-                  href="#pills-profile"
-                  role="tab"
-                  aria-controls="pills-profile"
-                  aria-selected="false"
-                  >My Transactions</a
-                >
-              </li>
-            </ul>
-            <div class="tab-content" id="pills-tabContent">
+        <div class="row row-dashboard">
+          <div class="container-fluid">
+            <div class="dashboard-heading mt-5">
               <div
-                class="tab-pane fade show active"
-                id="pills-home"
-                role="tabpanel"
-                aria-labelledby="pills-home-tab"
+                class="alert alert-info alert-dismissible fade show"
+                role="alert"
               >
-                <div
-                  class="section-content section-dashboard-home"
-                  data-aos="fade-up"
+                <p>
+                  <b>👋 Selamat Datang {{ user.name }}</b>
+                  Kamu bisa melihat detail transaksi anda di menu detail. happy
+                  shoping 😍 ✌️
+                </p>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="alert"
+                  aria-label="Close"
                 >
-                  <div class="container-fluid">
-                    <div class="dashboard-heading mt-5">
-                      <div
-                        class="alert alert-info alert-dismissible fade show"
-                        role="alert"
-                      >
-                        <p>
-                          <b>Selamat Datang {{ user.name }}</b>
-                          Jika anda sudah melakukan transaksi, silahkan tracking
-                          resi anda di menu My transactions
-                        </p>
-                        <button
-                          type="button"
-                          class="close"
-                          data-dismiss="alert"
-                          aria-label="Close"
-                        >
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div
-                        class="alert alert-warning alert-dismissible fade show"
-                        role="alert"
-                      >
-                        <p>
-                          Kami akan mengirimkan email notifikasi beserta detail
-                          pembayran ke email
-                          <b>{{ user.email }}</b> jika anda sudah melakukan
-                          proses pembayaran.
-                        </p>
-                        <button
-                          type="button"
-                          class="close"
-                          data-dismiss="alert"
-                          aria-label="Close"
-                        >
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </div>
-              <div
-                class="tab-pane fade"
-                id="pills-profile"
-                role="tabpanel"
-                aria-labelledby="pills-profile-tab"
-              >
-                <!-- transaction table -->
-                <div class="card rounded">
-                  <div>
-                    <h5 class="font-weight-bold">
-                      <i class="fas fa-shopping-cart mr-2"></i>My Order
-                    </h5>
-                    <table
-                      class="
-                        table table-striped table-bordered
-                        mt-4
-                        table-responsive-lg
-                      "
-                      v-if="orders.length > 0"
-                    >
-                      <thead class="text-uppercase thead-dark">
-                        <tr class="text-center">
-                          <th>invoice</th>
-                          <th>Full name</th>
-                          <th>Courier</th>
-                          <th>Status</th>
-                          <th>Grand Total</th>
-                          <th>Option</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr v-for="order in orders" :key="order.id">
-                          <td>{{ order.invoice }}</td>
-                          <td>{{ order.name }}</td>
-                          <td>
-                            {{ order.courier }} | {{ order.service }} | Rp.
-                            {{ moneyFormat(order.cost_courier) }}
-                          </td>
-                          <td>
-                            {{ order.status }}
-                          </td>
-                          <td>Rp. {{ moneyFormat(order.grand_total) }}</td>
-                          <td class="text-center">
-                            <router-link
-                              :to="{
-                                name: 'detail_order',
-                                params: { snap_token: order.snap_token },
-                              }"
-                              class="btn btn-secondary"
-                              ><i class="fas fa-eye mr-2"></i
-                              >detail</router-link
-                            >
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    <!-- data kosong -->
-                    <div v-else>
-                      <table
-                        class="
-                          table table-striped table-bordered
-                          mt-4
-                          table-responsive-lg
-                        "
-                      >
-                        <thead class="text-uppercase thead-dark">
-                          <tr class="text-center">
-                            <th>invoice</th>
-                            <th>Full name</th>
-                            <th>Shipping</th>
-                            <th>Grand Total</th>
-                            <th>Option</th>
-                          </tr>
-                        </thead>
-                      </table>
-                      <div class="alert alert-danger text-center h5">
-                        Data Kosong
-                      </div>
-                    </div>
-                    <hr />
+            </div>
+          </div>
+          <div class="col-12 mt-2">
+            <!-- transaction table -->
+            <div class="card rounded">
+              <div>
+                <h5 class="font-weight-bold">
+                  <i class="fas fa-shopping-cart mr-2"></i>My Order
+                </h5>
+                <table
+                  class="table table-striped mt-4 table-responsive-sm"
+                  v-if="orders.length > 0"
+                >
+                  <thead class="text-uppercase table-info table-hover">
+                    <tr class="text-center">
+                      <th>invoice</th>
+                      <th>Full name</th>
+                      <th>Courier</th>
+                      <th>Status</th>
+                      <th>Grand Total</th>
+                      <th>Option</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="order in orders" :key="order.id">
+                      <td>{{ order.invoice }}</td>
+                      <td>{{ order.name }}</td>
+                      <td>
+                        {{ order.courier }} | {{ order.service }} | Rp.
+                        {{ moneyFormat(order.cost_courier) }}
+                      </td>
+                      <td>
+                        {{ order.status }}
+                      </td>
+                      <td>Rp. {{ moneyFormat(order.grand_total) }}</td>
+                      <td class="text-center">
+                        <router-link
+                          :to="{
+                            name: 'detail_order',
+                            params: { snap_token: order.snap_token },
+                          }"
+                          class="btn btn-sm btn-info"
+                          ><i class="fas fa-eye mr-2"></i>detail</router-link
+                        >
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <!-- data kosong -->
+                <div v-else>
+                  <table
+                    class="
+                      table table-striped table-bordered
+                      mt-4
+                      table-responsive-lg
+                    "
+                  >
+                    <thead class="text-uppercase thead-dark">
+                      <tr class="text-center">
+                        <th>invoice</th>
+                        <th>Full name</th>
+                        <th>Shipping</th>
+                        <th>Grand Total</th>
+                        <th>Option</th>
+                      </tr>
+                    </thead>
+                  </table>
+                  <div class="alert alert-danger text-center h5">
+                    Data Kosong
                   </div>
                 </div>
+                <hr />
               </div>
             </div>
           </div>
@@ -200,11 +131,10 @@
 
 <style>
 .dashboard-customer {
-  padding-top: 140px;
+  padding-top: 100px;
 }
-
-.dashboard-content {
-  margin-top: 100px;
+.row-dashboard {
+  margin-top: -70px;
 }
 </style>
 
